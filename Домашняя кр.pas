1cc568e36bac69047d32d882afd2ced72f0f085e
Art-Cheater <118550801+Art-Cheater@users.noreply.github.com>
2022-11-18 18:49:06 +0300

    Add files via upload

---
+++ Домашняя кр.pas
@@ -0,0 +1,15 @@
+﻿var x,a,b:real;
+y,h:real;
+begin
+  writeln('Введите промежуток a и b и шаг h');
+  read(a,b,h);
+  x:=a;
+  while x<=b do 
+    begin
+  if x<-7 then y:=59
+  else if  (x<1) then y:=96
+  else y:=(power(x,1/3)/(x*x))*(tan(x)/sin(x));
+    writeln('x = ', x:1:2, ' y = ',y:1:2);
+    x+=h;
+    end;
+end.
